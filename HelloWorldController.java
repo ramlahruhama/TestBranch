85221c8612fe3439649182683d588507d693675d
ramlahruhama <rasheethali4u@gmail.com>
2018-02-02 10:46:15 -0500

    add all the files

---
+++ HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.rasheeth.webapps;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/")
+public class HelloWorldController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String welcome() {
+		return "welcomePage";
+		
+	}
+}
